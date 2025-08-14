85c85e1fcaa1191313bb452f2af01f8efe17dd36
James Hepburn <jameshepburn@MacBookPro.lan>
2025-08-14 10:36:01 -0600

    Initial commit

---
+++ src/main/java/com/example/bloggingplatformapi/BloggingplatformapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloggingplatformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingplatformapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingplatformapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bloggingplatformapi/BloggingplatformapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloggingplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingplatformapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
