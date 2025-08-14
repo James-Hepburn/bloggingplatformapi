0f36b5e0282a0e1d5bd688ec50c5002656dc9c38
James Hepburn <jameshepburn@MacBookPro.lan>
2025-08-14 11:38:31 -0600

    Final version

--- src/main/java/com/example/bloggingplatformapi/controller/PostController.java
+++ src/main/java/com/example/bloggingplatformapi/controller/PostController.java
@@ -2,9 +2,7 @@ package com.example.bloggingplatformapi.controller;
 
 import com.example.bloggingplatformapi.model.Post;
 import com.example.bloggingplatformapi.repository.PostRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,34 @@ public class PostController {
     @GetMapping
     public List <Post> getAllPosts () {
         return this.repo.findAll ();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPostById (@PathVariable Long id) {
+        Post post = repo.findById (id).orElseThrow (() -> new RuntimeException ("Post not found"));
+        return post;
+    }
+
+    @PostMapping
+    public Post createPost (@RequestBody Post post) {
+        return this.repo.save (post);
+    }
+
+    @PutMapping("/{id}")
+    public Post updatePost (@PathVariable Long id, @RequestBody Post post) {
+        Post oldPost = getPostById (id);
+
+        oldPost.setTitle (post.getTitle ());
+        oldPost.setContent (post.getContent ());
+        oldPost.setCategory (post.getCategory ());
+        oldPost.setTags (post.getTags ());
+
+        return this.repo.save (oldPost);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost (@PathVariable Long id) {
+        getPostById (id);
+        this.repo.deleteById (id);
     }
 }
